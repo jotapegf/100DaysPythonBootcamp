7b827b0b2e9cbee9e908d64398e801b92eeed130
JoaoHardline <joaohardline@gmail.com>
2024-05-06 10:06:16 -0300

    day 1 and 2, init of the repository

---
+++ Day1/bandNameGen.py
@@ -0,0 +1,10 @@
+print("Welcome to the band name generator")
+print("What's the name of the city you grew up?")
+city = input()
+# the computer ignores the " when we add a \
+# so we can print: What's "the" name of the city you grow up?
+print("What's the name of a cool pet? ")
+pet = input()
+bandName = city + " " + pet
+print("Your band name could be " + bandName)
+
